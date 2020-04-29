a9764b3f4d8d4839b04c77df7105baa03a382eed
Jhonny Camacho <killingjokewow@gmail.com>
2020-04-29 00:24:47 -0600

    Rails + React + Apollo + Graphql
    Rails + React + Apollo + Graphql Tutorial

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,17 @@
+module Types
+  class QueryType < Types::BaseObject
+    field :users, [UserType], null: false
+
+    def users
+      User.all
+    end
+
+    field :user, UserType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def user(id:)
+      User.find(id)
+    end
+  end
+end
